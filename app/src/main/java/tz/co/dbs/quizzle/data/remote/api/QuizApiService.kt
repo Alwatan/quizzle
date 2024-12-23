be022d757434bc64d822fa6f43f0c8ca41c094a4
Alwatan <pugudean@gmail.com>
2024-12-23 23:20:51 +0300

    setup retrofit service and question dto

--- app/src/main/java/tz/co/dbs/quizzle/data/remote/api/QuizApiService.kt
+++ app/src/main/java/tz/co/dbs/quizzle/data/remote/api/QuizApiService.kt
@@ -1,10 +1,11 @@
 package tz.co.dbs.quizzle.data.remote.api
 
 import retrofit2.http.GET
+import tz.co.dbs.quizzle.data.remote.dto.QuestionDto
 import javax.inject.Singleton
 
 @Singleton
 interface QuizApiService {
     @GET("questions.json")
-    suspend fun getQuestions(): List<Question>
+    suspend fun getQuestions(): List<QuestionDto>
 }
