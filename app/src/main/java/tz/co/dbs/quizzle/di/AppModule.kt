2e923c2c83f0595f7689a5c28ae653b5c72b4e25
Alwatan <pugudean@gmail.com>
2024-12-23 23:13:10 +0300

    setup hilt hilt module class

---
+++ app/src/main/java/tz/co/dbs/quizzle/di/AppModule.kt
@@ -0,0 +1,10 @@
+package tz.co.dbs.quizzle.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+}
